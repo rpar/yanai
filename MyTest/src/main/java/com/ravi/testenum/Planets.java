d31d2bf82f54bfae78213a05c1be2f9f111b7ed9
rpar <p_ravis@yahoo.com>
2015-11-02 14:37:26 -0800

    enum example

--- MyTest/src/main/java/com/ravi/testenum/Planets.java
+++ MyTest/src/main/java/com/ravi/testenum/Planets.java
@@ -7,7 +7,14 @@ interface Named {
 
 enum Planets implements Named {
     Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune;
-    // name() is implemented automagically.
+    // name() is implemented automatically.
     public int order() { return ordinal()+1; }
+    
+    public static void main(String[] args) {
+		for(Planets p : values())
+		{
+			System.out.println(p.order() + "-" + p);
+		}
+	}
 }
 
